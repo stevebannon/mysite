700a9acb064d2aa0c5479385e50a2b859844e44f
gamma1074 <steven.bannon@gmail.com>
2013-11-18 10:10:00 -0500

    Up to 404 errors in part 3

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,25 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from polls.models import Poll
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.  You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request,poll_id):
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request,poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request,poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
