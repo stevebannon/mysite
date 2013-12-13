54a3e7f5267aa9ae9f28d12062d232fc0b33b3ad
gamma1074 <steven.bannon@gmail.com>
2013-12-13 10:54:27 -0500

    Adding soenthing to the index template

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
-from polls import views
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('polls.urls', namespace="polls")),
 )
