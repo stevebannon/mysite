b0dbaa106cb09ba7fb85790f7c292d788f57b370
gamma1074 <steven.bannon@gmail.com>
2013-12-13 11:42:21 -0500

    converting to postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
