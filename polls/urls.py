361ce761eb391309d0fa6206b113aa487b8b662f
sbannon <sbannon@nist.gov>
2013-12-13 16:18:49 +0000

    fixing the urls.py for the polls app (hopefully)

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import patterns, url
+from polls import views
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('polls.urls', namespace="polls")),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
